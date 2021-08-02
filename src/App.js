06a8d2901589edd581b93aa57a955e1b8a1eba30
Aagam-Jain-113 <aagamjain113@gmail.com>
2021-08-02 22:23:17 +0530

    added: Preloader animation

--- src/App.js
+++ src/App.js
@@ -4,15 +4,36 @@ import Navbar from './components/Navbar';
 import Content from './Content';
 import Hero from './Hero';
 import Cursor from './Cursor';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 const App = () => {
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000)
+  });
   return (
     <div>
-      <Cursor />
-      <Navbar />
-      <Hero />
-      <Content />
-      <Footer />
+      {loading ?
+      <div className="flex items-center justify-center h-screen"> 
+      <Loader
+        type="Grid"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+      </div> 
+      : <div>
+        <Cursor />
+        <Navbar />
+        <Hero />
+        <Content />
+        <Footer />
+      </div>
+      }
     </div>
   );
 };
